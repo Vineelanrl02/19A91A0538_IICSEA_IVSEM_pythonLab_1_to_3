6e559054af4fdb99627a26a0a640d4a83ec23dd3
Vineelanrl02 <82566483+Vineelanrl02@users.noreply.github.com>
2021-08-09 14:39:49 +0530

    Update and rename 2)operations-(2.2) to 2)operations-(2.2).py

--- 2)operations-(2.2).py
+++ 2)operations-(2.2).py
@@ -9,3 +9,16 @@ output:
   2
   3
   sum is:  5
+  
+'''Implement a python script add.py that takes 2 numbers as command line arguments
+and perform arithmetic operations on them.(using putty)'''
+import sys as s
+print(int(s.argv[1])+int(s.argv[2]))
+
+OUTPUT:
+ [19A91A0538@Linux ~]$ vi cmd.py
+[19A91A0538@Linux ~]$ python3 cmd.py 2 3
+5
+
+
+
